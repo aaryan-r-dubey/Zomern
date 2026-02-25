758eaa05b2495bf3642f867c86a9e6fc018ffd1d
aaryan2604 <aaryan.dubey@somaiya.edu>
2026-02-25 12:34:02 +0530

    user regitration api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,12 @@
 const userModel=require("../models/user.model");
+const bcrypt=require("bcryptsjs");
+const jwt=require("jsonwebtoken");
 
 
 async function registerUser(req,res){
     
     const{fullName,email,password}=req.body;
+
     const isUserAlreadyExists=await userModel.findOne({
         email
     })
@@ -13,5 +16,28 @@ async function registerUser(req,res){
             message:"user already exists", 
         })
     }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const user=await userModel.create({
+        fullName,
+        email,
+        password:hashedPassword
+    })
+
+    const token=jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET)
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"user registered successfully",
+        user:{
+            id:user._id,
+            fullName:user.fullName,
+            email:user.email
+        }
+    })
 }
 
