a3998844f3e00747ef24482ecc077d649dbe697d
aaryan2604 <aaryan.dubey@somaiya.edu>
2026-03-01 14:51:16 +0530

    imagekit cloud and foodcreate api

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,5 +1,10 @@
 const express=require('express');
 const foodModel=require('../models/food.model');
+const storageService=require('../services/storage.service');
+const {v4:uuid}=require('uuid');
+
+
+
 
 async function createFood(req,res){
 
@@ -7,8 +12,31 @@ async function createFood(req,res){
 
    console.log(req.body);
    console.log(req.file);
+   console.log("BODY:", req.body);
+    console.log("DESCRIPTION:", req.body.description);
 
-   res.send("food created successfully");
+   const fileUploadResult=await storageService.uploadFile(req.file.buffer,uuid());
+
+   console.log(fileUploadResult);
+
+//    return res.json({
+//   name: req.body.name,
+//   description: req.body.description
+// });
+
+
+
+   const fooditem=await foodModel.create({
+    name:req.body.name,
+    description:req.body.description,
+    video:fileUploadResult.url,
+    foodPartner:req.foodPartner._id,
+   })
+
+   res.status(201).json({
+    message:"food item created successfully",
+    foodItem:fooditem,
+   })
 
 
 }
